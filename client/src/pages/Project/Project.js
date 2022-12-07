e20866008505e15bd4b2428959048e1db1c88312
RichardBass You <richardbassyou@RichardBasssMBP.lan>
2022-12-07 12:44:03 -0800

    add full container css class for app.js, and container UI for the website

---
+++ client/src/pages/Project/Project.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Container from '../../components/UI/Container';
+
+const Project = () => {
+    return (
+        <Container>
+            Project Page
+        </Container>
+    );
+};
+
+export default Project;
