6d5c4bf37047fe7a4dda52ae413e891b0dba539e
Crestatic <dtnguy86@gmail.com>
2022-12-07 19:00:10 -0800

    added schemas and auth for log in

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,39 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        addProject(savedProjects: myProject): User
+        removeProject(projectId: ID!): User
+    }
+
+    type: User {
+        _id: ID
+        username: String!
+        email: String!
+        password: String!
+        github: String
+        linkedin: String
+        myProjects: [Project]
+    }
+
+    type Project {
+        projectId: ID
+        projectName: String!
+        projectDescription: String!
+        task: String
+        colaborators: [User]
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+`;
+
+module.exports = typeDefs;
