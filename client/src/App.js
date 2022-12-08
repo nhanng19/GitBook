14eb6bd279f36eca3cd0d25cc49fda1e293279db
nhan <nhanng19@gmail.com>
2022-12-08 15:13:18 -0800

    changed background color

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import Landing from './components/Landing/Landing';
+import LandingPage from './pages/LandingPage';
 import Project from './pages/Project/Project';
 // import Container from './components/UI/Container';
 import './App.css';
@@ -12,7 +11,7 @@ function App() {
       <Router>
         <div className="full-container">
           <Routes>
-            <Route path="/" element={<Landing />} />
+            <Route path="/" element={<LandingPage/>} />
             {/* <Route path="/dashboard" element={<Dashboard />} /> */}
             {/* <Route path="/profile" element={<Profile />} /> */}
             <Route path="/project" element={<Project />} />
