0b92d409c4c7e16c7996eb41e0c39640e4f8df2e
nhan <nhanng19@gmail.com>
2022-12-08 18:38:26 -0800

    fixed bug

--- client/src/components/SignupModal/SignupModal.js
+++ client/src/components/SignupModal/SignupModal.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Form, Button, Alert } from "react-boostrap";
+import { Form, Button, Alert } from "react-bootstrap";
 import { useMutation } from "@apollo/client";
 import { ADD_USER } from "../../utils/mutations";
 import Auth from "../../utils/auth";
