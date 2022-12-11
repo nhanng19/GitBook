5b0d94c6254f697f190c221908757aecac6a4c2e
RichardBass You <richardbassyou@RichardBasss-MacBook-Pro.local>
2022-12-11 15:32:22 +0900

    refactored into Kanban.js

--- client/src/components/Project/ProjectView.js
+++ client/src/components/Project/ProjectView.js
@@ -1,6 +1,7 @@
 import React from "react";
 import classes from "./ProjectView.module.css";
 import { FaTrashAlt, FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import Kanban from "../UI/Kanban";
 
 const ProjectView = () => {
   return (
@@ -31,99 +32,7 @@ const ProjectView = () => {
           <button className={classes.addBtn}>Add</button>
         </div>
       </form>
-      <div className={classes.kanbanBox}>
-        <div className={classes.kanban}>
-          <div className={classes.kanbanHeader}>To-Do</div>
-          <div className={classes.kanbanBody}>
-            <div className={classes.kanbanItem}>
-              <div className={classes.itemHeader}>
-                <h3 className={classes.itemAssigner}>Richard</h3>
-                <button className={classes.btn}>
-                  <svg className={classes.icon}>
-                    <FaTrashAlt size="2rem" />
-                  </svg>
-                </button>
-                <button className={classes.btn}>
-                  <svg className={classes.icon}>
-                    <FaArrowRight size="2rem" />
-                  </svg>
-                </button>
-              </div>
-              <p className={classes.itemDescription}>Testing</p>
-            </div>
-
-            <div className={classes.kanbanItem}>
-              <div className={classes.itemHeader}>
-                <h3 className={classes.itemAssigner}>Richard</h3>
-                <button className={classes.btn}>
-                  <svg className={classes.icon}>
-                    <FaTrashAlt size="2rem" />
-                  </svg>
-                </button>
-                <button className={classes.btn}>
-                  <svg className={classes.icon}>
-                    <FaArrowRight size="2rem" />
-                  </svg>
-                </button>
-              </div>
-              <p className={classes.itemDescription}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Netus et malesuada fames ac turpis egestas integer eget. Sit
-                amet facilisis magna etiam
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className={classes.kanban}>
-          <div className={classes.kanbanHeader}>In-Progress</div>
-          <div className={classes.kanbanBody}>
-            <div className={classes.kanbanItem}>
-              <div className={classes.itemHeader}>
-                <h3 className={classes.itemAssigner}>Richard</h3>
-                <button className={classes.btn}>
-                  <svg className={classes.icon}>
-                    <FaArrowLeft size="2rem" />
-                  </svg>
-                </button>
-                <button className={classes.btn}>
-                  <svg className={classes.icon}>
-                    <FaArrowRight size="2rem" />
-                  </svg>
-                </button>
-              </div>
-              <p className={classes.itemDescription}>
-                Testing adsfasdghajshdfjadasfhk testubgajkshdgakjshgdlkjasdghklj
-                asdgagds
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className={classes.kanban}>
-          <div className={classes.kanbanHeader}>Done</div>
-          <div className={classes.kanbanBody}>
-            <div className={classes.kanbanItem}>
-              <div className={classes.itemHeader}>
-                <h3 className={classes.itemAssigner}>Richard</h3>
-                <button className={classes.btn}>
-                  <svg className={classes.icon}>
-                    <FaArrowLeft size="2rem" />
-                  </svg>
-                </button>
-
-                <button className={classes.btn}>
-                  <svg className={classes.icon}>
-                    <FaTrashAlt size="2rem" />
-                  </svg>
-                </button>
-              </div>
-              <p className={classes.itemDescription}>Testing</p>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Kanban />
       <div className={classes.chatBox}>
         <div className={classes.chat}>
           <p>Richard : blah blah blah</p>
