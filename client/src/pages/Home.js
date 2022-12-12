55b1353cc8e3207f4e3f0b356a6e8db279eff4a1
Crestatic <dtnguy86@gmail.com>
2022-12-11 17:40:25 -0800

    chat modal added

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,12 @@
 import React from "react";
 import Main from "../components/UI/Main";
-// import ProjectView from "../components/Project/ProjectView";
-import Chat from "../components/Chat/Chat";
+import ProjectView from "../components/Project/ProjectView";
+// import Chat from "../components/Chat/Chat";
 
 const Home = () => {
   return (
     <Main>
       <ProjectView />
-      
     </Main>
   );
 };
