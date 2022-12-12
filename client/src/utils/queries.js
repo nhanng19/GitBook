68751b3117b4999c97f90ae5f91672cdab70acb1
nhan <nhanng19@gmail.com>
2022-12-12 03:20:52 -0800

    refractored client queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,5 +1,36 @@
 import { gql } from '@apollo/client';
 
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      github
+      linkedin
+      projects {
+        projectName
+        projectDescription
+        projectOwner
+        createdAt
+        task
+      }
+    }
+  }
+`;
+
+export const QUERY_PROJECTS = gql`
+  query getProjects {
+    projects {
+      _id
+      projectName
+      projectDescription
+      projectRepo
+      projectOwner
+    }
+  }
+`;
+
 export const QUERY_ME = gql`
     query me {
         me {
@@ -8,11 +39,16 @@ export const QUERY_ME = gql`
             email
             github
             linkedin
-            myProjects {
+            projects {
+                _id
                 projectName
                 projectDescription
+                projectRepo
+                projectOwner
+                createdAt
                 task
             }  
         }
     }
 `;
+
