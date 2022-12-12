bc28b67a8d1952b225d9042c0e1ad6f056c8d66f
nhan <nhanng19@gmail.com>
2022-12-12 03:18:40 -0800

    active navigation functionality, navigation functionality

--- client/src/components/UI/SideBar.js
+++ client/src/components/UI/SideBar.js
@@ -8,24 +8,55 @@ import {
   FaSignOutAlt,
 } from "react-icons/fa";
 import SideBarItem from "./SideBarItem";
-
+import Auth from "../..//utils/auth.js";
+import { NavLink } from "react-router-dom";
 const SideBar = (props) => {
+
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
   return (
     <nav className={classes.sidebar}>
       <ul className={classes.side_nav}>
-        <SideBarItem name="HOME" href="#" >
-          <FaHome size="2rem" color="#333" />
-        </SideBarItem>
-        <SideBarItem name="DASHBOARD" href="#">
-          <FaClipboardCheck size="2rem" color="#333" />
-        </SideBarItem>
-        <SideBarItem name="PROFILE" href="#">
-          <FaFolder size="2rem" color="#333" />
-        </SideBarItem>
-        <SideBarItem name="DONATION" href="#">
-          <FaDonate size="2rem" color="#333" />
-        </SideBarItem>
-        <SideBarItem name="LOGOUT" href="#">
+        <NavLink
+          style={{ textDecoration: "none" }}
+          className={(navData) => (navData.isActive ? classes.active : "")}
+          to="/Home"
+        >
+          <SideBarItem name="HOME" href="">
+            <FaHome size="2rem" color="#333" />
+          </SideBarItem>
+        </NavLink>
+        <NavLink
+          style={{ textDecoration: "none" }}
+          className={(navData) => (navData.isActive ? classes.active : "")}
+          to="/Dashboard"
+        >
+          <SideBarItem name="DASHBOARD" href="">
+            <FaClipboardCheck size="2rem" color="#333" />
+          </SideBarItem>
+        </NavLink>
+        <NavLink
+          style={{ textDecoration: "none" }}
+          className={(navData) => (navData.isActive ? classes.active : "")}
+          to="/Profile"
+        >
+          <SideBarItem name="PROFILE" href="">
+            <FaFolder size="2rem" color="#333" />
+          </SideBarItem>
+        </NavLink>
+        <NavLink
+          style={{ textDecoration: "none" }}
+          className={(navData) => (navData.isActive ? classes.active : "")}
+          to="/Donation"
+        >
+          <SideBarItem name="DONATION" href="">
+            <FaDonate size="2rem" color="#333" />
+          </SideBarItem>
+        </NavLink>
+
+        <SideBarItem name="LOGOUT" onClick={logout} href="">
           <FaSignOutAlt size="2rem" color="#333" />
         </SideBarItem>
       </ul>
