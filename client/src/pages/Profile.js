adc5a21d934bef234302eaaff43e8c51d33ed220
nhan <nhanng19@gmail.com>
2022-12-13 09:44:26 -0800

    added react-scroll as a dependecy

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -3,7 +3,7 @@ import { Navigate, useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import AddProject from "../components/Project/AddProject";
 import ProjectList from "../components/Project/ProjectList";
-import { QUERY_USER, QUERY_ME } from "../utils/queries";
+import { QUERY_ME, QUERY_USER } from "../utils/queries";
 import Auth from "../utils/auth";
 
 const Profile = () => {
