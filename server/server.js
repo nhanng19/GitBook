9be38f7fcb2f43e56c5f92cc837f9e0b6fc76834
RichardBass You <richardbassyou@RichardBasss-MacBook-Pro.local>
2022-12-14 05:46:42 +0900

    this is not my commit, Dat's commit for setting up socket.io. I just fix something little

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const db = require("./config/connection");
-const formatMessage = require('./utils/message');
+const formatMessage = require("./utils/message");
 
 const { ApolloServer } = require("apollo-server-express");
 const { typeDefs, resolvers } = require("./schemas");
@@ -14,35 +14,19 @@ const PORT = process.env.PORT || 3001;
 
 // dependencies for socket.io
 const server = require("http").createServer(app);
-const io =  require("socket.io")(server);
-
-
-const startServer = async () => {
-
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: authMiddleware,
-  });
-  await server.start();
-  server.applyMiddleware({ app });
-  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-};
-
-startServer();
+// const io =  require("socket.io")(server);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
 app.get("/config", (req, res) => {
-  res.send ({
-    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY
-  })
+  res.send({
+    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  });
 });
 
 app.post("/create-payment-intent", async (req, res) => {
@@ -59,31 +43,56 @@ app.post("/create-payment-intent", async (req, res) => {
       error: {
         message: e.message,
       },
-    })
+    });
   }
 });
 
-db.once("open", () => {
-  server.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
-
-
-
-io.on("connection", socket => {
-
-  // Welcome current user
-  socket.broadcast.emit('message', formatMessage('ChatBot', 'Welcome to Chat'));
-
-  // Broadcast when a user connects
-  socket.broadcast.emit('message', formatMessage('ChatBott', 'A user has joined the chat'));
-
-  // Listen for ChatMessage
-  socket.on('chatMessage', (msg) => {
-    io.emit('message', formatMessage('USER', msg));
-  })
-
-  // Broadcast when a user disconnect
-  socket.on('disconnect', () => {
-    io.emit('message', formatMessage('ChatBot', 'A user has left the chat'));
+const startServer = async () => {
+  const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
   });
-});
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
+
+  db.once("open", () => {
+    const io = require("socket.io")(server);
+    io.on("connection", (socket) => {
+      // Welcome current user
+      socket.broadcast.emit(
+        "message",
+        formatMessage("ChatBot", "Welcome to Chat")
+      );
+
+      // Broadcast when a user connects
+      socket.broadcast.emit(
+        "message",
+        formatMessage("ChatBott", "A user has joined the chat")
+      );
+
+      // Listen for ChatMessage
+      socket.on("chatMessage", (msg) => {
+        socket.broadcast.emit("message", formatMessage("USER", msg));
+      });
+
+      // Broadcast when a user disconnect
+      socket.on("disconnect", () => {
+        io.emit(
+          "message",
+          formatMessage("ChatBot", "A user has left the chat")
+        );
+      });
+    });
+
+    server.listen(PORT, () =>
+      console.log(`🌍 Now listening on localhost:${PORT}`)
+    );
+  });
+
+  console.log(
+    `Use GraphQL at http://localhost:${PORT}${apolloServer.graphqlPath}`
+  );
+};
+
+startServer();
