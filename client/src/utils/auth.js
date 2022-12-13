11190605ccbf8beb5d78e9e2ef17adec1beaa66a
nhan <nhanng19@gmail.com>
2022-12-13 04:07:52 -0800

    set time out to allow API to fetch, then reassign location

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,47 +1,45 @@
-import decode from 'jwt-decode';
-
+import decode from "jwt-decode";
 // create a new class to instantiate for a user
 class AuthService {
-    getProfile() {
-        return decode(this.getToken());
-    }
+  getProfile() {
+    return decode(this.getToken());
+  }
 
-    // checking if user is logged in
-    loggedIn() {
-        const token = this.getToken();
-        return !!token && !this.isTokenExpired(token);
-    }
+  // checking if user is logged in
+  loggedIn() {
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
+  }
 
-    isTokenExpired(token) {
-        try {
-            const decoded = decode(token);
-            if (decoded.exp < Date.now() / 1000) {
-                return true;
-            } else return false;
-        } catch (err) {
-            return false;
-        }
+  isTokenExpired(token) {
+    try {
+      const decoded = decode(token);
+      if (decoded.exp < Date.now() / 1000) {
+        return true;
+      } else return false;
+    } catch (err) {
+      return false;
     }
+  }
 
-    getToken() {
-        // get token from local storage
-        return localStorage.getItem('id_token');
-    }
+  getToken() {
+    // get token from local storage
+    return localStorage.getItem("id_token");
+  }
 
-    login(idToken) {
-         // save token to local storage
-         localStorage.setItem('id_token', idToken);
-         window.location.assign('/Profile');
-     }
+  login(idToken) {
+    // save token to local storage
+      localStorage.setItem("id_token", idToken);
+    setTimeout(() => {
+      window.location.assign("/Profile");
+    }, 2000);
+  }
 
-    logout() {
-        // clear token and profile
-        localStorage.removeItem('id_token');
-        window.location.assign('/')
-    }
+  logout() {
+    // clear token and profile
+    localStorage.removeItem("id_token");
+    window.location.assign("/");
+  }
 }
 
 export default new AuthService();
-
-
-
