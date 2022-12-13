10872a66b84ed880647250df3020ab6375ef2211
nhan <nhanng19@gmail.com>
2022-12-13 04:08:11 -0800

    remove repoProjects prop

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,12 +1,9 @@
 import React from "react";
 import { Navigate, useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
-import search from "../utils/API";
 import AddProject from "../components/Project/AddProject";
 import ProjectList from "../components/Project/ProjectList";
-import { useState, useEffect } from "react";
 import { QUERY_USER, QUERY_ME } from "../utils/queries";
-
 import Auth from "../utils/auth";
 
 const Profile = () => {
@@ -15,17 +12,10 @@ const Profile = () => {
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam },
   });
-  const [projects, setProjects] = useState("");
+
   const user = data?.me || data?.user || {};
 
-  const searchRepo = async (query) => {
-    const response = await search(query);
-    setProjects(response.data);
-  };
 
-  useEffect(() => {
-    searchRepo(user.username);
-  }, [user.username]);
   // navigate to personal profile page if username is yours
   if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
     return <Navigate to="/Profile" />;
@@ -39,7 +29,6 @@ const Profile = () => {
     <div>
       {!userParam && <AddProject />}
       <ProjectList
-        repoProjects={projects}
         projects={user.projects}
         title={user.username}
         showUsername={false}
