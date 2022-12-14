035a06861957581dcc0928290b041485cda3dc8d
nhan <nhanng19@gmail.com>
2022-12-13 22:12:57 -0800

    refractored project view and kanban board and stylized

--- client/src/components/Project/ProjectView.js
+++ client/src/components/Project/ProjectView.js
@@ -1,32 +1,12 @@
 import React from "react";
-import classes from "./ProjectView.module.css";
-import Container from "../UI/Container";
-import ProjectInfo from "./ProjectInfo";
 import Kanban from "../Kanban/Kanban";
 
-const ProjectView = () => {
+const ProjectView = ({ name, description, date, owner,repo }) => {
   const DUMMY_DESCRIPTION = `project description test, dummy description.`;
   return (
-    <Container>
-      <ProjectInfo
-        title="project title"
-        date="2022/12/27"
-        description={DUMMY_DESCRIPTION}
-      />
-      <Kanban />
-      <div className={classes.chatBox}>
-        <div className={classes.chat}>
-          <p>Richard : blah blah blah</p>
-          <p>Nhan : lahb lahb lahb</p>
-          <p>Dat : ahbl ahbl ahbl</p>
-          <p>Lydia : hbla hbla hbla</p>
-        </div>
-        <form className={classes.chatForm}>
-          <textarea className={classes.chatInput}></textarea>
-          <button className={` ${classes.sendBtn}`}>Send</button>
-        </form>
-      </div>
-    </Container>
+    <>
+      <Kanban name={name} description={description} date={date} owner={owner} repo={repo} />
+    </>
   );
 };
 
