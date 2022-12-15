79075e4b820f92d64b1944d7741de3f1ba3fd05f
nhan <nhanng19@gmail.com>
2022-12-15 03:38:18 -0800

    added loading animation while home is fetching data

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,16 +3,19 @@ import ProjectView from "../components/Project/ProjectView";
 import { QUERY_PROJECTS } from "../utils/queries";
 import ProjectList from "../components/Project/ProjectList";
 import { useQuery } from "@apollo/client";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
 const Home = () => {
   const { loading, data } = useQuery(QUERY_PROJECTS);
   const projects = data?.projects || [];
   return (
-    <ProjectList
-      projects={projects}
-      title="All GitBook"
-    />
+    <>
+      {loading ? (
+        <LoadingSpinner />
+      ) : (
+        <ProjectList projects={projects} title="All GitBook" />
+      )}
+    </>
   );
 };
 
 export default Home;
-
