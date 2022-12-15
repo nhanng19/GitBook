00d37113a555acabddca28813bd6f1f63f031ed1
Lydia Kim <lydiakim47@gmail.com>
2022-12-14 21:45:20 -0800

    nodemailer sending emails

--- server/server.js
+++ server/server.js
@@ -16,45 +16,45 @@ const cors = require("cors");
 
 app.use(cors());
 
-let transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    type: "OAuth2",
-    user: process.env.EMAIL,
-    pass: process.env.WORD,
-    clientId: process.env.OAUTH_CLIENTID,
-    clientSecret: process.env.OAUTH_CLIENT_SECRET,
-    refreshToken: process.env.OAUTH_REFRESH_TOKEN,
-  },
- });
+// let transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     type: "OAuth2",
+//     user: process.env.EMAIL,
+//     pass: process.env.WORD,
+//     clientId: process.env.OAUTH_CLIENTID,
+//     clientSecret: process.env.OAUTH_CLIENT_SECRET,
+//     refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+//   },
+//  });
 
- transporter.verify((err, success) => {
-  err
-    ? console.log(err)
-    : console.log(`=== Server is ready to take messages: ${success} ===`);
- });
+//  transporter.verify((err, success) => {
+//   err
+//     ? console.log(err)
+//     : console.log(`=== Server is ready to take messages: ${success} ===`);
+//  });
 
- app.post("/send", function (req, res) {
-  let mailOptions = {
-    from: process.env.EMAIL,
-    to: `${req.body.userData.email}`,
-    subject: "Gitbook Registration",
-    text: "Hello there! Welcome to Gitbook! Begin your exciting journey of collaborating with fellow developers now!",
-  };
+//  app.post("/", function (req, res) {
+//   let mailOptions = {
+//     from: process.env.EMAIL,
+//     to: `${req.body.userData.email}`,
+//     subject: "Gitbook Registration",
+//     text: "Hello there! Welcome to Gitbook! Begin your exciting journey of collaborating with fellow developers now!",
+//   };
  
-  transporter.sendMail(mailOptions, function (err, data) {
-    if (err) {
-      res.json({
-        status: "fail",
-      });
-    } else {
-      console.log("== Message Sent ==");
-      res.json({
-        status: "success",
-      });
-    }
-  });
- });
+//   transporter.sendMail(mailOptions, function (err, data) {
+//     if (err) {
+//       res.json({
+//         status: "fail",
+//       });
+//     } else {
+//       console.log("== Message Sent ==");
+//       res.json({
+//         status: "success",
+//       });
+//     }
+//   });
+//  });
 
 // dependencies for socket.io
 const server = require("http").createServer(app);
@@ -84,6 +84,45 @@ const startApolloServer = async (typeDefs, resolvers) => {
     const server = app.listen(PORT, () =>
       console.log(`🌍 Now listening on localhost:${PORT}`)
     );
+    let transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: process.env.EMAIL,
+        pass: process.env.WORD,
+        clientId: process.env.OAUTH_CLIENTID,
+        clientSecret: process.env.OAUTH_CLIENT_SECRET,
+        refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+      },
+     });
+    
+     transporter.verify((err, success) => {
+      err
+        ? console.log(err)
+        : console.log(`=== Server is ready to take messages: ${success} ===`);
+     });
+    
+     app.post("/", function (req, res) {
+      let mailOptions = {
+        from: process.env.EMAIL,
+        to: `${req.body.userData.email}`,
+        subject: "Gitbook Registration",
+        text: "Hello there! Welcome to Gitbook! Begin your exciting journey of collaborating with fellow developers now!",
+      };
+     
+      transporter.sendMail(mailOptions, function (err, data) {
+        if (err) {
+          res.json({
+            status: "fail",
+          });
+        } else {
+          console.log("== Message Sent ==");
+          res.json({
+            status: "success",
+          });
+        }
+      });
+     });
     const io = require("socket.io")(server);
     // Connecting Socket IO
     io.on("connection", (socket) => {
