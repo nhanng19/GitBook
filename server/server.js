780adffec8316bce6b74088e1cdd7c75437fe42b
nhan <nhanng19@gmail.com>
2022-12-21 16:15:13 -0800

    heroku deploy

--- server/server.js
+++ server/server.js
@@ -32,6 +32,10 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
 // Starting Apollo Server
 const startApolloServer = async (typeDefs, resolvers) => {
   const apolloServer = new ApolloServer({
